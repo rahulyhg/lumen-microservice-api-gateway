3ebec0d21cbdf0a84550a663d185d17a4f1f93c7
Martin Macharia <mosmartin5@gmail.com>
2019-04-22 20:55:27 +0200

    Update web.php
    Add the author routes

--- routes/web.php
+++ routes/web.php
@@ -15,6 +15,15 @@
 //     return $router->app->version();
 // });
 
+// author routes
+$router->get('/authors', 'AuthorController@index');
+$router->post('/authors', 'AuthorController@store');
+$router->get('/authors/{author}', 'AuthorController@show');
+$router->put('/authors/{author}', 'AuthorController@update');
+$router->patch('/authors/{author}', 'AuthorController@update');
+$router->delete('/authors/{author}', 'AuthorController@destroy');
+
+// book routes
 $router->get('/books', 'BookController@index');
 $router->post('/books', 'BookController@store');
 $router->get('/books/{book}', 'BookController@show');
